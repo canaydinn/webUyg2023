93bd7c62c26fde5e234f96fed94b8f5e181dfe1c
canaydinn <canaydinn@gmail.com>
2023-12-14 15:44:39 +0300

    grafik statik olarak yapıldı

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const dbConn=require("../db/mysql_connect")
 const bcrypt=require("bcrypt")
+const Response=require("../utils/response")
 const login=async(req,res)=>{
     const kullanici_adi=req.body.kullanici_adi
     const sifre=req.body.sifre
@@ -8,10 +9,7 @@ const login=async(req,res)=>{
         if(results.length>0){
             const comparePassword=bcrypt.compare(sifre,results[0].sifre)
             if(comparePassword){
-                return res.status(203).json({
-                    success:true,
-                    message:"Kullanıcı Girişi Başarılı"
-                })
+               return new Response(results).basarili_giris(res)
             }else{
                 return res.status(203).json({
                     success:false,
@@ -38,18 +36,12 @@ const register=async(req,res)=>{
    const dogum_tarihi=req.body.dogum_tarihi
    dbConn.query("select * from kullanicilar where kullanici_adi=?",kullanici_adi,(err,result)=>{
     if(result.length>0){
-        return res.status(201).json({
-            success:false,
-            message:"Kayıt Mevcut"
-        })
+        return new Response(result,"Böyle bir kayıt var").duplicated(res)
     }else{
         dbConn.query("INSERT INTO kullanicilar (kullanici_adi,sifre,eposta,adi,soyadi,tel_no,cinsiyet,dogum_tarihi) VALUES (?,?,?,?,?,?,?,?)"
         ,[kullanici_adi,sifre,eposta,adi,soyadi,tel_no,cinsiyet,dogum_tarihi],(err,result)=>{
             if(!err){
-                return res.status(201).json({
-                    success:true,
-                    message:"Kayıt Başarılı"
-                })
+                return new Response(result,"Succesful").created(res)
             }else{
                 console.log(err)
             }
@@ -60,4 +52,18 @@ const register=async(req,res)=>{
    })
 }
 
-module.exports={login,register}
+const  getChartData=(req,res)=>{
+        const data={
+            labels:["Ocak","Şubat","Mart","Nisan","Mayıs"],
+            datasets:[{
+                label:"Aylık Satışlar",
+                data:[65,59,22,45,38],
+                backgroundColor:'rgbd(75,192,192,0.2)',
+                borderColor:'rgba(75,192,192,1)',
+                borderWidth:1
+            }]
+        }
+        res.json(data)
+    }
+
+module.exports={login,register,getChartData}
--- routers/routes.js
+++ routers/routes.js
@@ -1,8 +1,8 @@
 const router=require('express').Router()
-const {login,register}=require('../controllers/controller')
+const {login,register,getChartData}=require('../controllers/controller')
 router.post("/login",login)
-
 router.post("/register",register)
+router.get("/grafik",getChartData)
 //post:veri gönderme get:veri alma put:veri güncelleme patch:veri güncelleme
 //delete:veri silme
 module.exports=router
